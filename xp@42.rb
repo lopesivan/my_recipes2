96aeb13da4432154623f5fba268374cf5459e168
Ivan Lopes <lopesivan.ufrj@gmail.com>
2024-09-27 12:23:45 -0300

    Atualiza o arquivo xp@42.rb

--- xp@42.rb
+++ xp@42.rb
@@ -17,21 +17,21 @@ class XpAT42 < Formula
   depends_on 'redis-bash' => :build
 
   def install
-    inreplace 'tasks/add.sh', '__REDIS_BASH_LIB__', '/home/linuxbrew/.linuxbrew/opt/redis-bash/redis-bash-lib
-    inreplace 'tasks/cat.sh', '__REDIS_BASH_LIB__', '/home/linuxbrew/.linuxbrew/opt/redis-bash/redis-bash-lib
-    inreplace 'tasks/copy.sh', '__REDIS_BASH_LIB__', '/home/linuxbrew/.linuxbrew/opt/redis-bash/redis-bash-lib
-    inreplace 'tasks/c.sh', '__REDIS_BASH_LIB__', '/home/linuxbrew/.linuxbrew/opt/redis-bash/redis-bash-lib
-    inreplace 'tasks/e.sh', '__REDIS_BASH_LIB__', '/home/linuxbrew/.linuxbrew/opt/redis-bash/redis-bash-lib
-    inreplace 'tasks/i.sh', '__REDIS_BASH_LIB__', '/home/linuxbrew/.linuxbrew/opt/redis-bash/redis-bash-lib
-    inreplace 'tasks/keys.sh', '__REDIS_BASH_LIB__', '/home/linuxbrew/.linuxbrew/opt/redis-bash/redis-bash-lib
-    inreplace 'tasks/link.sh', '__REDIS_BASH_LIB__', '/home/linuxbrew/.linuxbrew/opt/redis-bash/redis-bash-lib
-    inreplace 'tasks/ls.sh', '__REDIS_BASH_LIB__', '/home/linuxbrew/.linuxbrew/opt/redis-bash/redis-bash-lib
-    inreplace 'tasks/ls.sh.OLD', '__REDIS_BASH_LIB__', '/home/linuxbrew/.linuxbrew/opt/redis-bash/redis-bash-lib
-    inreplace 'tasks/pop.sh', '__REDIS_BASH_LIB__', '/home/linuxbrew/.linuxbrew/opt/redis-bash/redis-bash-lib
-    inreplace 'tasks/push.sh', '__REDIS_BASH_LIB__', '/home/linuxbrew/.linuxbrew/opt/redis-bash/redis-bash-lib
-    inreplace 'tasks/rm.sh', '__REDIS_BASH_LIB__', '/home/linuxbrew/.linuxbrew/opt/redis-bash/redis-bash-lib
-    inreplace 'tasks/s.sh', '__REDIS_BASH_LIB__', '/home/linuxbrew/.linuxbrew/opt/redis-bash/redis-bash-lib
-    inreplace 'tasks/x.sh', '__REDIS_BASH_LIB__', '/home/linuxbrew/.linuxbrew/opt/redis-bash/redis-bash-lib
+    inreplace 'tasks/add.sh', '__REDIS_BASH_LIB__', '/home/linuxbrew/.linuxbrew/opt/redis-bash/redis-bash-lib'
+    inreplace 'tasks/cat.sh', '__REDIS_BASH_LIB__', '/home/linuxbrew/.linuxbrew/opt/redis-bash/redis-bash-lib'
+    inreplace 'tasks/copy.sh', '__REDIS_BASH_LIB__', '/home/linuxbrew/.linuxbrew/opt/redis-bash/redis-bash-lib'
+    inreplace 'tasks/c.sh', '__REDIS_BASH_LIB__', '/home/linuxbrew/.linuxbrew/opt/redis-bash/redis-bash-lib'
+    inreplace 'tasks/e.sh', '__REDIS_BASH_LIB__', '/home/linuxbrew/.linuxbrew/opt/redis-bash/redis-bash-lib'
+    inreplace 'tasks/i.sh', '__REDIS_BASH_LIB__', '/home/linuxbrew/.linuxbrew/opt/redis-bash/redis-bash-lib'
+    inreplace 'tasks/keys.sh', '__REDIS_BASH_LIB__', '/home/linuxbrew/.linuxbrew/opt/redis-bash/redis-bash-lib'
+    inreplace 'tasks/link.sh', '__REDIS_BASH_LIB__', '/home/linuxbrew/.linuxbrew/opt/redis-bash/redis-bash-lib'
+    inreplace 'tasks/ls.sh', '__REDIS_BASH_LIB__', '/home/linuxbrew/.linuxbrew/opt/redis-bash/redis-bash-lib'
+    inreplace 'tasks/ls.sh.OLD', '__REDIS_BASH_LIB__', '/home/linuxbrew/.linuxbrew/opt/redis-bash/redis-bash-lib'
+    inreplace 'tasks/pop.sh', '__REDIS_BASH_LIB__', '/home/linuxbrew/.linuxbrew/opt/redis-bash/redis-bash-lib'
+    inreplace 'tasks/push.sh', '__REDIS_BASH_LIB__', '/home/linuxbrew/.linuxbrew/opt/redis-bash/redis-bash-lib'
+    inreplace 'tasks/rm.sh', '__REDIS_BASH_LIB__', '/home/linuxbrew/.linuxbrew/opt/redis-bash/redis-bash-lib'
+    inreplace 'tasks/s.sh', '__REDIS_BASH_LIB__', '/home/linuxbrew/.linuxbrew/opt/redis-bash/redis-bash-lib'
+    inreplace 'tasks/x.sh', '__REDIS_BASH_LIB__', '/home/linuxbrew/.linuxbrew/opt/redis-bash/redis-bash-lib'
 
     system 'make', "prefix=#{prefix}", 'install'
   end

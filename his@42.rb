5c43545fb5ade3b2a001d7ebd56eedbcaa2410d0
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-07-24 08:50:04 -0300

    Atualiza o arquivo his@42.rb

--- his@42.rb
+++ his@42.rb
@@ -2,15 +2,16 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class HisAt42 < Formula
-  desc "A command history utility with icons and colors that works on Windows and GNU/Linux.
-"
-  homepage "https://terminalroot.com.br/2025/07/veja-o-historico-do-terminal-de-forma-interativa.html"
-  url "https://github.com/terroo/his.git",
-      branch: "main",
-      revision: "695c51a145f9911735b61fb673241e190ea23be3"
-  version "0.8.5"
-  head "https://github.com/terroo/his.git", branch: "main"
-  license "GPL-3.0-or-later"
+  desc "A command history utility with icons and colors that works on Windows and GNU/Linux."
+  homepage 'https://terminalroot.com.br/2025/07/veja-o-historico-do-terminal-de-forma-interativa.html'
+  url 'https://github.com/terroo/his.git',
+      branch: 'main',
+      using: :git
+  version '1.0.0'
+  head 'https://github.com/terroo/his.git',
+       branch: 'main',
+       using: :git
+  license 'GPL-3.0-or-later'
 
   def install
     system "cmake", ".", "-B", "build"

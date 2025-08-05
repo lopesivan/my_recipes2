16e6aed9a45c1a82da62037d4eaa36f4285bef3a
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-08-05 05:38:34 -0300

    [1.0.1] update st-autotools@42.rb

--- st-autotools@42.rb
+++ st-autotools@42.rb
@@ -4,17 +4,17 @@
 
 class StAutotoolsAT42 < Formula
   desc "st is a simple terminal implementation for X."
-  homepage "git@github.com:lopesivan/st-autotools.git"
-  url "git@github.com:lopesivan/st-autotools.git.git",
+  homepage "https://github.com/lopesivan/st-autotools"
+  url "https://github.com/lopesivan/st-autotools.git",
     revision: "c68401fde9c25b94e72ca7f68d1513dc4349713a"
   version "1.0.1"
-#  head "git@github.com:lopesivan/st-autotools.git",
+#  head "https://github.com/lopesivan/st-autotools",
 #    branch: "main",
 #    using: :git
   license "GPL-3.0-or-later"
   patch do
-    url 'https://raw.githubusercontent.com/lopesivan/my_patches/main/st-autotools/st-autotools-custom-c08febe4704ded4ad927b0576e578d24fb0c5133-20250805.diff'
-    sha256 'b5f9ee24a0a3e79d9639522b257ca11227d4a404e9be40a47062e137e1cae918'
+    url 'https://raw.githubusercontent.com/lopesivan/my_patches/main/st-autotools/st-autotools-custom-c6774ff2465faecadf91d5acaa6d143199dab8fe-20250805.diff'
+    sha256 '27dc447edcc69331cb202c65ebf635d320a4f61fe1efe9913c1b4802eea1a243'
   end
   def install
     system './gera-opcao.sh', 'enable', 'debug', '"Ativa modo de depuração"'

5ac4ae90732e53372fd2b21d146f96f2d87e023b
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-08-05 08:21:41 -0300

    [1.0.1] update st-autotools@42.rb

--- st-autotools@42.rb
+++ st-autotools@42.rb
@@ -13,15 +13,19 @@ class StAutotoolsAT42 < Formula
 #    using: :git
   license "GPL-3.0-or-later"
   patch do
-    url 'https://raw.githubusercontent.com/lopesivan/my_patches/main/st-autotools/st-autotools-custom-f4e14aed9a6ba58d7db1e42e5166971fe7eab4ff-20250805.diff'
-    sha256 '76723bf63f78a0c0dde1d077c75b68b55ac6fda3b991741569e42019676c7f59'
+    url 'https://raw.githubusercontent.com/lopesivan/my_patches/main/st-autotools/st-autotools-custom-b892f44bc6dbe39257ddc8a0856f52518fcad8f8-20250805.diff'
+    sha256 'dc37eff9369a28b7205c8b0323f3bd31bba95fb5460162af7f3db12560177441'
   end
     def install
-      # Força o uso do pkg-config do sistema
-      ENV["PKG_CONFIG"] = "/usr/bin/pkg-config"
   
-      # Força o path onde estão os .pc files
-      ENV["PKG_CONFIG_PATH"] = "/usr/lib/x86_64-linux-gnu/pkgconfig"
+      ENV["PKG_CONFIG_PATH"] = [
+      Formula["freetype"].opt_lib/"pkgconfig",
+      Formula["fontconfig"].opt_lib/"pkgconfig",
+      Formula["libpng"].opt_lib/"pkgconfig",
+      Formula["zlib"].opt_lib/"pkgconfig",
+      Formula["bzip2"].opt_lib/"pkgconfig",
+      Formula["expat"].opt_lib/"pkgconfig"
+      ].join(":")
   
       system "./gera-opcao.sh", "enable", "debug", '"Ativa modo de depuração"'
       system "sh", "autogen.sh"

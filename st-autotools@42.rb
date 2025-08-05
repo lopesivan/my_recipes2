883077d282a1c2e1e45218341abd2817a742bf53
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-08-05 07:37:49 -0300

    [1.0.1] update st-autotools@42.rb

--- st-autotools@42.rb
+++ st-autotools@42.rb
@@ -6,15 +6,15 @@ class StAutotoolsAT42 < Formula
   desc "st is a simple terminal implementation for X."
   homepage "https://github.com/lopesivan/st-autotools"
   url "https://github.com/lopesivan/st-autotools.git",
-    revision: "9c32113c8db90db10fa7e29b087515e2b2c776b6"
+    revision: "41de93778bc76679a94fc96ac74f10941470e655"
   version "1.0.1"
 #  head "https://github.com/lopesivan/st-autotools",
 #    branch: "main",
 #    using: :git
   license "GPL-3.0-or-later"
   patch do
-    url 'https://raw.githubusercontent.com/lopesivan/my_patches/main/st-autotools/st-autotools-custom-3104e1876c087da58d4e36d02a03a7da5dc64f63-20250805.diff'
-    sha256 '5d83c036f7914ada9b63099ceb94436f861fbf0cf8365f79596bd748d36ca870'
+    url 'https://raw.githubusercontent.com/lopesivan/my_patches/main/st-autotools/st-autotools-custom-f4e14aed9a6ba58d7db1e42e5166971fe7eab4ff-20250805.diff'
+    sha256 '76723bf63f78a0c0dde1d077c75b68b55ac6fda3b991741569e42019676c7f59'
   end
     def install
       # Força o uso do pkg-config do sistema

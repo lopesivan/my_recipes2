e4156cd15bc75348f97fd423ba0be835b77ccd61
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-10-23 16:41:53 -0300

    [1.0.4] update hw@42.rb

--- hw@42.rb
+++ hw@42.rb
@@ -7,11 +7,15 @@ class HwAT42 < Formula
   homepage "https://github.com/lopesivan/hw"
   url "https://github.com/lopesivan/hw.git",
     revision: "bce43e5943d7221b095bcbdbd0d6b23c39fe64b8"
-  version "1.0.3"
+  version "1.0.4"
 #  head "https://github.com/lopesivan/hw",
 #    branch: "main",
 #    using: :git
   license "GPL-3.0-or-later"
+  patch do
+    url 'https://raw.githubusercontent.com/lopesivan/my_patches/main/hw/hw-custom-35e227f25cd0e90b03edc280f0392e9ff6048c0f-20251023.diff'
+    sha256 '6ca7444de16c8e03b0f120465d880a6b4544dfc09ddabc748af8f364b47ae730'
+  end
   def install
     args = %W[
       --no-warn-unused-cli
@@ -21,14 +25,13 @@ class HwAT42 < Formula
       -DCMAKE_CXX_COMPILER:FILEPATH=clang++
       -DCMAKE_INSTALL_PREFIX=#{prefix}
     ]
-
-    mkdir "build" do
-      system "cmake", *args, "..", "-G", "Ninja"
-      system "ninja"
-      system "ninja", "install"
+  
+    mkdir("build") do
+      system("cmake", *args, "..", "-G", "Ninja")
+      system("ninja")
+      system("ninja", "install")
     end
   end
-
   test do
     system "false"
   end

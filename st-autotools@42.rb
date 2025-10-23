531727955909718cb0f67cdd8280ab1168e54305
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-10-23 17:43:06 -0300

    [1.0.1] update st-autotools@42.rb

--- st-autotools@42.rb
+++ st-autotools@42.rb
@@ -13,8 +13,8 @@ class StAutotoolsAT42 < Formula
 #    using: :git
   license "GPL-3.0-or-later"
   patch do
-    url 'https://raw.githubusercontent.com/lopesivan/my_patches/main/st-autotools/st-autotools-custom-4ec7c545ead5555bfa18371ad4ecb098c7849c03-20251023.diff'
-    sha256 'df5ca4857a13452cc1a37d7305041eff0aadc572323c9c576cfb6e8444900f73'
+    url 'https://raw.githubusercontent.com/lopesivan/my_patches/main/st-autotools/st-autotools-custom-fc211b92f3d302b27bde93906a182fe4b66dfd73-20251023.diff'
+    sha256 '4fd0711c3e83be168ee2614df34c9297ef29b3be345465e4ce5ad6ad89f2bd3e'
   end
   def install
     system("aclocal", "-I", "m4", "--install")

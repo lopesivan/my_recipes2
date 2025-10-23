4e8648f49508f7a730b817b9266b74d09030dba8
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-10-23 15:19:43 -0300

    [1.0.1] update hw@42.rb

--- hw@42.rb
+++ hw@42.rb
@@ -13,10 +13,13 @@ class HwAT42 < Formula
 #    using: :git
   license "GPL-3.0-or-later"
   patch do
-    url 'https://raw.githubusercontent.com/lopesivan/my_patches/main/hw/hw-custom-26e513b092a2185c9ca29bfaddc1467a5a07812c-20251023.diff'
-    sha256 '4580a46523afe71589b0ffaf13278b8306d1a2b95c7a7d990ead500f1880a131'
+    url 'https://raw.githubusercontent.com/lopesivan/my_patches/main/hw/hw-custom-a64330c4c77b768042a78e19c784efeaea3c3e50-20251023.diff'
+    sha256 'eaaa59fd7fc28ac0f679f4d08ce3490619c64500aa6c15f3f0d33c160647c717'
   end
-  
+    def install
+      system 'cmake', '.'
+      system 'make', "PREFIX=#{prefix}", 'install'
+    end
   test do
     system "false"
   end
